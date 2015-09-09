378547d877748bed99740d6dad24afcf046d249a
Rasmus Holm <rasho381@li15-15.ida.liu.se>
2015-09-09 19:06:07 +0200

    Lab1 almost done

--- lab1/Lab1.pl
+++ lab1/Lab1.pl
@@ -20,12 +20,27 @@ kind(bosse).
 like(X,Y):- male(X), female(Y),	beautiful(Y).
 like(X,Y):- =(nisse,X), female(Y), like(Y,X).
 
-like(X,Y):- =(ulrika,X),like(Y,X), male(Y), rich(Y), kind(Y).
-like(X,Y):- =(ulrika,X),like(Y,X) ,male(Y), beautiful(Y), strong(Y).
+like(X,Y):- =(ulrika,X), male(Y), rich(Y), kind(Y), like(Y,X).
+like(X,Y):- =(ulrika,X), male(Y), beautiful(Y), strong(Y), like(Y,X).
 
 happy(X):- rich(X).
 happy(X):- male(X), female(Y), like(X,Y), like(Y,X).
 happy(X):- female(X), male(Y), like(X,Y), like(Y,X).
 
+edge(a,b).
+edge(a,c).
+edge(b,c).
+edge(c,d).
+edge(c,e).
+edge(e,f).
+edge(e,g).
+edge(f,g).
+edge(d,h).
+edge(d,f).
 
-
+path(X,Y):- edge(X,Y).
+path(X,Y):- edge(X,Z), path(Z,Y). 
+path(X,Y,[X,Y]):- edge(X,Y).
+path(X,Y,[X,Z|XS]):- edge(X,Z), path(Z,Y,XS).
+%npath(X,Y,[Len,X,Y]):-  Len is 2-1,edge(X,Y).
+npath(X,Y,[Len,[X,LS|XS]]):- path(X,Y,XS),length(XS,Len).
