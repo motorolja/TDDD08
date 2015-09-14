50d12a6b78cae79c88808e783aca61e16691781f
Rasmus Holm <rasho381@li10-6.ida.liu.se>
2015-09-14 16:59:54 +0200

    Did insert sort

--- lab2/lab2.pl
+++ lab2/lab2.pl
@@ -1,7 +1,14 @@
 /* Lab 2 TDDD08 - Logic Programming */
 
-list(6,2,3,1,9,5,7).
+numbers([6,2,3,1,9,2,5,7]).
 
-isort(X,Y):- 1.
+isort(Unsorted,Sorted):- insert_sort([6,2,3,1,9,2,5,7],[],Sorted). 
+insert_sort([],Acc,Acc).
 
-qsort(X,Y):- 2.
+insert_sort([Head|Tail],Acc,Sorted):- insert_element(Head,Acc,NAcc), insert_sort(Tail,NAcc,Sorted).
+
+insert_element(X,[Y|Tail],[Y|NT]):- X>Y, insert_element(X,Tail,NT).
+insert_element(X,[Y|Tail],[X,Y|Tail]):- X=<Y.
+insert_element(X,[],[X]).
+
+% qsort(X,[]):- 2.
