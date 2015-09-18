57ecbfc4ee4e95afcf1487b89cd9feae9e22b9fe
Rasmus <rasmusholm957@hotmail.com>
2015-09-18 12:35:29 +0200

    Fixed insertion sort and quick sort

--- lab2/lab2.pl
+++ lab2/lab2.pl
@@ -1,19 +1,6 @@
 /* Lab 2 TDDD08 - Logic Programming */
 
-%numbers([6,2,3,1,9,2,5,7]).
-
-%isort(Unsorted,Sorted):- insert_sort([6,2,3,1,9,2,5,7],[],Sorted). 
-%insert_sort([],Acc,Acc).
-
-%insert_sort([Head|Tail],Acc,Sorted):- insert_element(Head,Acc,NAcc), insert_sort(Tail,NAcc,Sorted).
-
-%insert_element(X,[Y|Tail],[Y|NT]):- X>Y, insert_element(X,Tail,NT).
-%insert_element(X,[Y|Tail],[X,Y|Tail]):- X=<Y.
-%insert_element(X,[],[X]).
-
-% qsort(X,[]):- 2.
-
-%isort([Head|Tail],Sorted):- insert_element(Head,Tail,Sorted).
+/* Insertion sort */
 
 isort([],[]).
 isort([Head|Tail],Sorted):- isort(Tail,SortedTail),insert_element(Head,SortedTail,Sorted).
@@ -21,3 +8,25 @@ isort([Head|Tail],Sorted):- isort(Tail,SortedTail),insert_element(Head,SortedTai
 insert_element(X,[Y|Tail],[Y|NotTail]):- X>Y, insert_element(X,Tail,NotTail).
 insert_element(X,[Y|Tail],[X,Y|Tail]):- X=<Y.
 insert_element(X,[],[X]).
+
+/* Quick sort */
+qsort([],[]).
+qsort([Head|Tail],Sorted):-
+	splice_list(Head,Tail,LH,RH), qsort(LH,ListL), qsort(RH,ListR), append(ListL,[Head|ListR],Sorted).
+
+splice_list(_Value,[],[],[]).
+splice_list(Value,[Head|Tail],[Head|TailL],RH):- Value>Head,splice_list(Value,Tail,TailL,RH).
+splice_list(Value,[Head|Tail],LH,[Head|TailR]):- Value=<Head,splice_list(Value,Tail,LH,TailR).
+
+
+/* Exercise 2.2 - example code */
+% middle(X,Xs)
+% X is the middle element in the list Xs
+middle(X, [X]).
+middle(X, [_First|Xs]) :-
+append(Middle, [_Last], Xs),
+middle(X, Middle).
+
+
+
+
