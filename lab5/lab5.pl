288b721ea3a4be070df39c8525085d53c5e2a798
Phunkie92 <henrik_phung@hotmail.com>
2015-10-26 16:56:14 +0100

    lab5 - still not working with constraints

--- lab5/lab5.pl
+++ lab5/lab5.pl
@@ -1,27 +1,45 @@
-:- use_module(library(clpfd)).
+?- use_module(library(clpfd)).
 
 container(a,2,2).
 container(b,4,1).
 container(c,2,2).
 container(d,1,1).
 
-on(a,d).
-on(b,c).
-on(c,d).
+on(1,4).
+on(2,3).
+on(3,4).
 
+schedule(Ss, Result) :-
+	Ss = [S1,S2,S3,S4],
+	REnd = [R1,R2,R3,R4],
+	domain(Ss, 1, 30),
+	domain(REnd,1,50),
+	domain([Resources], 1,50),
+	Tasks = [task(S1,2,R1,2,1),
+		 task(S2,1,R2,4,2),
+		 task(S3,2,R3,2,3),
+		 task(S4,1,R4,1,4)],
+	maximum(Resources,REnd),
+	%order(Ss),
+	cumulative(Tasks, [limit(4)]),
+	
+	append(Ss, [Resources], Vars),
+	labeling([minimize(Resources)], Vars),
+	max(REnd,R),
+	min(Ss,S),
+	Result is (R-S)*4.
 
-compatible(ContainerA,ContainerB) :-
-	\+ on(ContainerA,ContainerB).
+order([]).
+order([X]).
+order([H1,H2|T]):-
+	(\+ on(H1,H2) -> order(T); (\+ on(H2,H1) -> order(T))).
 
-shortest_path([LH,RH|Tail],Out):-
-	compatible(LH,RH),
+checkblock(X, [H|List]):- 
 
-schedule(List, Resources):-
-	List = [A,B,C,D,E],
-	Tasks = [task(A,1,_,2,0),
-		 task(B,3,_,2,0),
-		task(C,4,_,2,0),
-		task(D,5,_,2,0),
-		task(E,6,_,2,0)],
-	cumulative(Tasks, [limit(6)]),
-	label('Starting').
+max([X],X).
+max([X|Xs],X):- max(Xs,Y), X >=Y.
+max([X|Xs],N):- max(Xs,N), N > X.
+
+min([X],X).
+min([X|Xs],X):- min(Xs,Y), X =<Y.
+min([X|Xs],N):- min(Xs,N), N < X.
