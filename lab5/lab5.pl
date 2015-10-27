260f3673056bf09defcaca8fa3a3771bdec6664d
Phunkie92 <henrik_phung@hotmail.com>
2015-10-27 11:38:37 +0100

    lab5.pl

--- lab5/lab5.pl
+++ lab5/lab5.pl
@@ -1,45 +1,44 @@
-?- use_module(library(clpfd)).
+:- use_module(library(clpfd)).
 
 container(a,2,2).
 container(b,4,1).
 container(c,2,2).
 container(d,1,1).
 
+:- dynamic
+	on/2.
 on(1,4).
-on(2,3).
-on(3,4).
+on(2,1).
+on(3,2).
 
-schedule(Ss, Result) :-
-	Ss = [S1,S2,S3,S4],
-	REnd = [R1,R2,R3,R4],
-	domain(Ss, 1, 30),
-	domain(REnd,1,50),
-	domain([Resources], 1,50),
-	Tasks = [task(S1,2,R1,2,1),
-		 task(S2,1,R2,4,2),
-		 task(S3,2,R3,2,3),
-		 task(S4,1,R4,1,4)],
-	maximum(Resources,REnd),
-	%order(Ss),
-	cumulative(Tasks, [limit(4)]),
+schedule(S, Done):-
+	S= [S1,S2,S3,S4],
+	E= [E1,E2,E3,E4],
+	D= [D1,D2,D3,D4],
+	domain(S, 0, 100),
+	domain(E, 0, 100),
+	D=[1,2,3,4],
+	Done in 1..100,
+	ready(E,Done),
+	Tasks = [task(S1,2,E1,2,D1),
+		 task(S2,1,E2,4,D2),
+		 task(S3,2,E3,2,D3),
+		 task(S4,1,E4,1,D4)],
 	
-	append(Ss, [Resources], Vars),
-	labeling([minimize(Resources)], Vars),
-	max(REnd,R),
-	min(Ss,S),
-	Result is (R-S)*4.
+	cumulative(Tasks, [limit(4)]),
+	%constrain(D),
+	labeling([minimize(Done)], [Done|S]).
 
-order([]).
-order([X]).
-order([H1,H2|T]):-
-	(\+ on(H1,H2) -> order(T); (\+ on(H2,H1) -> order(T))).
 
-checkblock(X, [H|List]):- 
 
-max([X],X).
-max([X|Xs],X):- max(Xs,Y), X >=Y.
-max([X|Xs],N):- max(Xs,N), N > X.
-
-min([X],X).
-min([X|Xs],X):- min(Xs,Y), X =<Y.
-min([X|Xs],N):- min(Xs,N), N < X.
+constrain([]).
+constrain([X]):- retract(on(X,Y)).
+constrain([H1,H2|Tail]):-
+	\+ on(H2,H1),
+	constrain([H1|Tail]),
+	constrain([H2|Tail]).
+		
+ready([], _).
+ready([E|Es], Done) :-
+	Done #>= E,
+	ready(Es, Done).
