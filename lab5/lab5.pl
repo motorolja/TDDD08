d9b694354c36ea7a632bf03e0b900eac603bf2cf
Rasmus Holm <rasho381@li0-9.ida.liu.se>
2015-10-27 16:05:05 +0100

    lab5 working on constraints

--- lab5/lab5.pl
+++ lab5/lab5.pl
@@ -8,8 +8,8 @@ container(d,1,1).
 :- dynamic
 	on/2.
 on(1,4).
-on(2,1).
-on(3,2).
+on(2,3).
+on(3,4).
 
 schedule(S, Done):-
 	S= [S1,S2,S3,S4],
@@ -20,6 +20,7 @@ schedule(S, Done):-
 	D=[1,2,3,4],
 	Done in 1..100,
 	ready(E,Done),
+	start(S,E),
 	Tasks = [task(S1,2,E1,2,D1),
 		 task(S2,1,E2,4,D2),
 		 task(S3,2,E3,2,D3),
@@ -37,8 +38,25 @@ constrain([H1,H2|Tail]):-
 	\+ on(H2,H1),
 	constrain([H1|Tail]),
 	constrain([H2|Tail]).
+	
 		
 ready([], _).
 ready([E|Es], Done) :-
 	Done #>= E,
 	ready(Es, Done).
+
+start([_X],[_Y]).
+start([S1,S2|STail],[E1,E2|ETail]):-
+	(evaluate(S1,S2,E1,E2) -> start([S2|STail],[E2|ETail]);
+	start([S2|STail],[E2|ETail])).
+
+evaluate(S1,S2,E1,E2):-
+	on(S1,S2)->S2#>=E1.
+	%on(S2,S1)->S1#>=E2).
+	
+
+reverse([],[]).
+reverse([X|Xs],YsX) :-
+	reverse(Xs,Ys),
+	append(Ys,[X],YsX).
+%ontop(X,Y):- on
