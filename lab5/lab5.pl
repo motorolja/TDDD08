c00d94209c1132afbd297bb8768667f0f50593a6
Rasmus Holm <rasho381@li2-6.ida.liu.se>
2015-10-29 17:56:37 +0100

    fixed lab5

--- lab5/lab5.pl
+++ lab5/lab5.pl
@@ -1,43 +1,40 @@
 :- use_module(library(clpfd)).
 
-container(a,2,2).
-container(b,4,1).
-container(c,2,2).
-container(d,1,1).
+/* container(Identifier,Resources,Time) */
+container(1,2,2).
+container(2,4,1).
+container(3,2,2).
+container(4,1,1).
 
-:- dynamic
-        on/2.
+/* on(ContainerA,ContainerB) */
 on(1,4).
 on(2,3).
 on(3,4).
 
 
-
-schedule(S, Done):-
+schedule(S, Sum):-
         findall([X,Y,Z], container(X,Y,Z), Containers),
-        to_list(Containers, R, D),
+        to_list(Containers, R, D, I),
         length(R, N),
         length(S, N),
         length(E, N),
         domain(S, 0, 100),
         domain(E, 0, 100),
         Done in 1..100,
+	Limit in 1..100,
         ready(E,Done),
-        start(S,E),
-        /*Tasks = [task(S1,2,E1,2,0),
-                 task(S2,1,E2,4,0),
-                 task(S3,2,E3,2,0),
-                 task(S4,1,E4,1,0)],*/
+        start(S,E,I),
         tasks(S, D, E, R, Tasks),
-        cumulative(Tasks, [limit(4)]),
-        labeling([minimize(Done)], [Done|S]).
+        cumulative(Tasks, [limit(Limit)]),
+	Sum #= Limit*Done,
+        labeling([minimize(Sum)], [Sum|S]).
 
-to_list([[X,Y,Z]|Containers], [Y|R], [Z|D]):- to_list(Containers, R, D).
-to_list([], [], []).
+to_list([[X,Y,Z]|Containers], [Y|R], [Z|D], [X|I]):- to_list(Containers, R, D, I).
+to_list([], [], [], []).
 
-constrain(S1, S2, E1, E2):- on(S1, S2), S2 #>= E1.
-constrain(S1, S2, E1, E2):- on(S2, S1), S1 #>= E2.
-constrain(S1, S2, E1, E2):- \+(on(S1, S2)), \+(on(S2, S1)).
+constrain(S1, S2, E1, E2, I1, I2):- on(I1, I2), S2 #>= E1.
+constrain(S1, S2, E1, E2, I1, I2):- on(I2, I1), S1 #>= E2.
+constrain(S1, S2, E1, E2, I1, I2):- \+(on(I1, I2)), \+(on(I2, I1)).
 
 
 ready([], _).
@@ -45,11 +42,11 @@ ready([E|Es], Done) :-
         Done #>= E,
         ready(Es, Done).
 
-start([],[]).
-start([S1|STail],[E1|ETail]):- start2(S1, STail, E1, ETail), start(STail, ETail).
+start([],[],[]).
+start([S1|STail],[E1|ETail],[I1|ITail]):- start2(S1, STail, E1, ETail, I1, ITail), start(STail, ETail, ITail).
 
-start2(X, [], Y, []).
-start2(S1, [S2|STail], E1, [E2|ETail]):- constrain(S1, S2, E1, E2), start2(S1, STail, E1, ETail).
+start2(X, [], Y, [], Z, []).
+start2(S1, [S2|STail], E1, [E2|ETail], I1, [I2|ITail]):- constrain(S1, S2, E1, E2, I1, I2), start2(S1, STail, E1, ETail, I1, ITail).
 
 tasks([], [], [], [], []).
 tasks([S|Ss], [D|Ds], [E|Es], [R|Rs], [T|Ts]) :-
